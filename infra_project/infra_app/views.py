155c328c9515f7710a87acfe0ba2c0bcbbb7abdf
newchipo <newchipo@gmail.com>
2023-05-21 11:39:38 +0400

    Add * to allowed 3 try

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось с третьей попытки!')
 
 
 def second_page(request):
